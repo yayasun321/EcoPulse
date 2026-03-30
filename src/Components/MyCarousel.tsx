387297134373c6a6ace05b8f8a6375f953595800
Pia-S <piasheth@outlook.com>
2026-03-30 11:54:11 -0700

    new formatting of carousel
    - replaced carousel images with background + text
    - added css file to format carousel slides

--- src/Components/MyCarousel.tsx
+++ src/Components/MyCarousel.tsx
@@ -1,19 +1,50 @@
 import Carousel from "react-bootstrap/Carousel";
-import slide1 from "../assets/Images/Slide1.png";
-import slide2 from "../assets/Images/Slide2.png";
-import slide3 from "../assets/Images/Slide3.png";
+
+import background from "../assets/Images/background.png";
+import "./MyCarousel.css";
 
 function MyCarousel() {
   return (
-    <Carousel>
+    <Carousel data-bs-theme="dark">
       <Carousel.Item>
-        <img src={slide1} />
+        <div className="slide-text">
+          <h5>test</h5>
+        </div>
+        <img src={background} />
+
+        <Carousel.Caption>
+          <h1>Welcome to...</h1>
+          <h3>Our website designed for your responsible shopping</h3>
+        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img src={slide2} />
+        <img src={background} />
+        <Carousel.Caption>
+          <h1>Our Mission?</h1>
+          <h3>
+            To promote responsible consumption by limiting impulse-buying and
+            encouraging long-term use of sustainable products.
+          </h3>
+          <h3>Targeting UN Goal 12: Sustainable Consumption by 2050</h3>
+          <p>So yes, your use of our website can make a difference</p>
+        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img src={slide3} />
+        <img src={background} />
+        <Carousel.Caption>
+          <h1>Why should I use this site?</h1>
+          <h3>
+            Every time you decide to purchase a product, do you wonder if it’s
+            worthwhile? Do you use them often? Or do these purchases just pile
+            up?
+          </h3>
+          <h3>
+            In our website, simply input the URL of your purchase and our AI
+            chat-box will ask personalized questions to help you evaluate your
+            purchase and give possible alternatives.
+          </h3>
+          <p>It’ll save the environment and your wallet!</p>
+        </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
   );
